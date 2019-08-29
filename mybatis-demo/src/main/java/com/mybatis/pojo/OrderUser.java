957a3706a5fd7e29fe3e8f4e3c2748c74751508c
朱运升 <19172222727@qq.com>
2019-08-29 19:20:57 +0800

    20190829学习

---
+++ mybatis-demo/src/main/java/com/mybatis/pojo/OrderUser.java
@@ -0,0 +1,86 @@
+package com.mybatis.pojo;
+
+import java.util.Date;
+
+/**
+ * @Author: zhuyunsheng
+ * @Date: 2019/8/29 19:18
+ */
+public class OrderUser extends Order {
+  private String userName;
+  private String password;
+  private String name;
+  private Integer age;
+  private Integer sex;
+  private Date birthday;
+  private Date created;
+  private Date updated;
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public Integer getSex() {
+    return sex;
+  }
+
+  public void setSex(Integer sex) {
+    this.sex = sex;
+  }
+
+  public Date getBirthday() {
+    return birthday;
+  }
+
+  public void setBirthday(Date birthday) {
+    this.birthday = birthday;
+  }
+
+  @Override
+  public Date getCreated() {
+    return created;
+  }
+
+  @Override
+  public void setCreated(Date created) {
+    this.created = created;
+  }
+
+  @Override
+  public Date getUpdated() {
+    return updated;
+  }
+
+  @Override
+  public void setUpdated(Date updated) {
+    this.updated = updated;
+  }
+}
